9b3f1cf5357eae591bf4311eaaff02ae4262b37b
Jacob <jacobhandley50@gmail.com>
2019-01-29 15:37:16 -0500

    Updated Tx file setup

--- Arduino/Transmitter/Transmitter.ino
+++ Arduino/Transmitter/Transmitter.ino
@@ -1,13 +1,27 @@
 //Code for the In Ear Monitor project
 
-//The WiFi library
+//The WiFi library we'll be using
 #include <WiFi.h>
 
+//Network credentials
+const char* ssid = "In-Ear Transmitter";
+const char* password = "123456789";
 
+//Set server port number to 80
+WiFiServer server(80);
 
 void setup() {
-  
+  Serial.begin(115200);
 
+  //Activate WiFi network
+  Serial.print("Setting up AP. . .");
+  Wifi.softAP(ssid, password);
+    //We can remove the passwor parameter if we want the network to be open
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("IP address: ");
+  Serial.println(IP);
+
+  server.begin();
 }
 
 void loop() {
