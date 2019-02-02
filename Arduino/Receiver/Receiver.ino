e428c5621052fc4c49b913f80a3d72771383dcc9
Jacob <jacobhandley50@gmail.com>
2019-02-02 13:50:48 -0500

    Updated Tx code  file, started RX coding, wrote a very basic Python server for testing.

---
+++ Arduino/Receiver/Receiver.ino
@@ -0,0 +1,11 @@
+//Code for the In Ear Monitor receiver
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
--- Arduino/Transmitter/Transmitter.ino
+++ Arduino/Transmitter/Transmitter.ino
@@ -1,7 +1,11 @@
-//Code for the In Ear Monitor project
+//Code for the In Ear Monitor transmitter
 
-//The WiFi library we'll be using
 #include <WiFi.h>
+#include <SPI.h>
+
+//Sets the SPI settings for reading from the ADC
+SPISettings adcSettings(705600, MSBFIRST, SPI_MODE0);
+  //These values are rough educated guesses, will adjust once we have the hardware
 
 //Network credentials
 const char* ssid = "In-Ear Transmitter";
@@ -12,7 +16,8 @@ WiFiServer server(80);
 
 void setup() {
   Serial.begin(115200);
-
+ 
+  //WIFI SETUP SECTION
   //Activate WiFi network
   Serial.print("Setting up AP. . .");
   WiFi.softAP(ssid, password);
@@ -20,17 +25,33 @@ void setup() {
   IPAddress IP = WiFi.softAPIP();
   Serial.print("IP address: ");
   Serial.println(IP);
-
   server.begin();
+
+  //SPI SETUP SECTION
+  pinMode(SS, OUTPUT);
+  //Ensures the ADC starts disabled (high signal disables devices)
+  digitalWrite(SS, HIGH);
+  SPI.begin();
+  
+  
 }
 
 void loop() {
   WiFiClient client = server.available();     //Listen for devices
+  uint8_t audio;
 
   if(client){
     Serial.println("Connected.");
     while(client.connected()){
-      //Send audio data to the client
+      SPI.beginTransaction(adcSettings);
+      //Enable the ADC
+      digitalWrite(SS, LOW);
+      //stores the received data in variable "audio"
+      //The data sent doesn't matter, as we are reading only
+      audio = SPI.transfer16(0);
+      digitalWrite(SS, HIGH);
+      SPI.endTransaction();
+      client.write(audio);    //This likely won't be the way it actually works
     }
     //Close the connection
     client.stop();
