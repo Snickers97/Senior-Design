7a7680e69e2b0b0733816d9acdd17a8bbc57ab24
Jacob <jacobhandley50@gmail.com>
2019-04-04 12:04:27 -0400

    adcTransmit

---
+++ Arduino/adcTransmitTest_jacob/adcTransmitTest_jacob.ino
@@ -0,0 +1,62 @@
+
+#include <WiFi.h>
+#include <driver/adc.h>   //allows use of onboard adc for testing
+#include "driver/spi_master.h"  //allow use of spi master, is master
+#include "driver/i2s.h" 
+#include "driver/adc.h" //allow use of on board adc
+
+//Network credentials
+const char* ssid = "In-Ear-Transmitter";
+const char* password = "123456789";
+
+int bufsize = 128;
+
+//Set server port number to 80
+WiFiServer server(80);
+
+void setup() {
+  
+  Serial.begin(115200);
+ 
+  //WIFI SETUP SECTION
+    //Activate WiFi network
+  Serial.print("Setting up AP. . .");
+  WiFi.softAP(ssid, password);
+    //We can remove the password parameter if we want the network to be open
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("IP address: ");
+  Serial.println(IP);
+  server.begin();
+  
+  //ADC config
+  adc1_config_width(ADC_WIDTH_BIT_12);  //configure precision
+  adc1_config_channel_atten(ADC1_CHANNEL_0,ADC_ATTEN_DB_0); //0<=V_in<=1.1 @0dB attenution
+    //ADC1_CHANNEL_0 => GPIO 36
+}
+
+void loop() {
+  WiFiClient client = server.available();     //Listen for devices
+
+  if(client){
+    Serial.println("Connected.");
+    client.setTimeout(20);
+    while(client.connected()){
+//      digitalWrite(26, HIGH);
+      uint16_t val = 0xFF00;
+      uint8_t audio;
+      audio = (val >> 4);
+      /*for(int i = 0; i < bufsize; i++){
+        val = adc1_get_raw(ADC1_CHANNEL_0); //read in from ADC
+        audio[i] = (val >> 4);
+      }*/
+//    
+     // client.write(audio,bufsize);
+      Serial.println(audio);
+//      digitalWrite(26, LOW);
+      delay(1000);
+    }
+    //Close the connection
+    client.stop();
+    Serial.println("Disconnected.");
+  }
+}
